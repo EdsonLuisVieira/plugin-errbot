9c76f5c029e927a4c9f2022cbf944e4ec704fe4b
Edson Luis Vieira de Almeida <edson.l.v.almeida@outlook.com>
2020-09-02 23:50:50 -0300

    ajuste02

--- pg-getlogs/showok.py
+++ pg-getlogs/showok.py
@@ -5,4 +5,4 @@ class Showok(BotPlugin):
     @botcmd()  # flags a command
     def showok(self, msg, args):  # a command callable with !tryme
         """ validando comentario2 """
-        return ("plugin "+args[1] + " ok"+ args[0])
+        return ("plugin "+ args + " ok"+ args[0])
