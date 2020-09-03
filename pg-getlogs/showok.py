f908d794a70b7a6ed8bb0d30131d82f25d217485
Edson Luis Vieira de Almeida <edson.l.v.almeida@outlook.com>
2020-09-02 23:12:41 -0300

    first commit

---
+++ pg-getlogs/showok.py
@@ -0,0 +1,7 @@
+from errbot import BotPlugin, botcmd
+
+class Showok(BotPlugin):
+
+    @botcmd  # flags a command
+    def showok(self, msg, args):  # a command callable with !tryme
+        return ("plugin ok")
